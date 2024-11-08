6aea44c1661b43beb56acfcba0e64ced00540b64
edijajuli <edijajuli1@gmail.com>
2024-11-08 22:03:18 +0700

    change ref() to reactive in JobListings.vue

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { reactive, onMounted } from "vue";
 import { RouterLink } from "vue-router";
 import JobListing from "@/components/JobListing.vue";
 import axios from "axios";
@@ -11,14 +11,19 @@ defineProps({
     default: false,
   },
 });
-const jobs = ref([]);
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+});
 
 onMounted(async () => {
   try {
     const response = await axios.get("http://localhost:5000/jobs");
-    jobs.value = response.data;
+    state.jobs = response.data;
   } catch (error) {
     console.error("Error fetching jobs", error);
+  } finally {
+    state.isLoading = false;
   }
 });
 </script>
@@ -31,7 +36,7 @@ onMounted(async () => {
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
-          v-for="job in jobs.slice(0, limit || jobs.length)"
+          v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
           :job="job"
         />
